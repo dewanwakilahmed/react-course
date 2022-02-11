1ca25eb9848bfcf9066aff1598c00c6dae898158
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-02-12 00:45:32 +0600

    Working with "Event Props" (Passing Function as Props)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,6 +10,10 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -19,8 +23,10 @@ function Todo(props) {
         </button>
       </div>
       {/* {modalIsOpen ? <Modal /> : null} */}
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
